8ba928026c920e5b4e7185b854ab5e611925658f
thyagostall <thstall@gmail.com>
2016-10-30 23:19:13 -0200

    Login and owner for snippets

--- django_rest_tutorial/urls.py
+++ django_rest_tutorial/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url, include
 
 
 urlpatterns = [
-    url(r'^', include('snippets.urls'))
+    url(r'^', include('snippets.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
